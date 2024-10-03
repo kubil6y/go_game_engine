2ccee5aea42de76299e85cb3dc3e16809d0939c1
kubil6y <lieqb2@gmail.com>
2024-10-03 21:18:04 +0300

    add asset store

---
+++ pkg/asset_store/asset_store.go
@@ -0,0 +1,58 @@
+package asset_store
+
+import (
+	"github.com/veandco/go-sdl2/img"
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+type AssetID int
+
+type AssetStore struct {
+	textures map[AssetID]*sdl.Texture
+}
+
+func New() *AssetStore {
+	return &AssetStore{
+		textures: make(map[AssetID]*sdl.Texture),
+	}
+}
+
+func (s *AssetStore) AddTexture(renderer *sdl.Renderer, assetID AssetID, filepath string) error {
+	surface, err := img.Load(filepath)
+	if err != nil {
+		return err
+	}
+	defer surface.Free()
+
+	texture, err := renderer.CreateTextureFromSurface(surface)
+	if err != nil {
+		return err
+	}
+
+	s.textures[assetID] = texture
+	return nil
+}
+
+func (s *AssetStore) GetTexture(assetID AssetID) (*sdl.Texture, bool) {
+	texture, exists := s.textures[assetID]
+	return texture, exists
+}
+
+func (s *AssetStore) GetOrLoadTexture(renderer *sdl.Renderer, assetID AssetID, filepath string) (*sdl.Texture, error) {
+	texture, exists := s.textures[assetID]
+	if exists {
+		return texture, nil
+	}
+	err := s.AddTexture(renderer, assetID, filepath)
+	if err != nil {
+		return nil, err
+	}
+	return s.textures[assetID], nil
+}
+
+func (s *AssetStore) Clear() {
+    for assetID, texture := range s.textures {
+        texture.Destroy()
+        delete(s.textures, assetID)
+    }
+}
