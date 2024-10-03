790c6929aa115bfeedc67c9e7c80674e9ca596c1
kubil6y <lieqb2@gmail.com>
2024-10-03 11:01:34 +0300

    remove set from System entities and use a slice

--- pkg/ecs/system.go
+++ pkg/ecs/system.go
@@ -2,20 +2,32 @@ package ecs
 
 import (
 	"github.com/kubil6y/go_game_engine/pkg/bitset"
-	"github.com/kubil6y/go_game_engine/pkg/set"
 )
 
 type System struct {
 	componentSignature bitset.Bitset32
-	entities           set.Set[Entity]
+	entities           []Entity
 }
 
 func (s *System) AddEntityToSystem(entity Entity) {
-	s.entities.Add(entity)
+	s.entities = append(s.entities, entity)
 }
 
 func (s *System) RemoveEntityFromSystem(entity Entity) {
-	s.entities.Remove(entity)
+	index := -1
+	for i := 0; i < len(s.entities); i++ {
+		if s.entities[i].GetID() == entity.GetID() {
+			index = i
+			break
+		}
+	}
+	if index != -1 {
+		s.entities = append(s.entities[:index], s.entities[index+1:]...)
+	}
+}
+
+func (s *System) GetSystemEntities() []Entity {
+	return s.entities
 }
 
 func (s *System) GetSignature() bitset.Bitset32 {
